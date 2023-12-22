fbde1ec93e87f9f112b666a2f8da7daccbc1f548
Moe3 <holdermoe@gmail.com>
2023-12-22 08:00:01 -0500

    Create MQG.cpp

---
+++ MQG.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    string questions[] = {"1. What is Moe's favorite Color?: ",
+                            "2. What is Moe's favorite Professional Athlete?: ",
+                            "3. Who is Moe's favorite Artist?: ",
+                            "4. How many siblings does Moe have?: "};
+                            
+    string choices[][4] = {{"A. Black", "B. Purple", "C. White", "D. Gray"},
+                            {"A. Lebron James", "B. Jalen Hurts", "C. Devin Haney", "D. Anthony Davis"},
+                            {"A. Kevin Gate", "B. Beyonce", "C. J Cole", "D. DMX"},
+                            {"A. 1", "B. 2","C. 3","D. 4"}};
+                            
+    char answersKey[] = {'A', 'A', 'A', 'D'};
+    
+    int size = sizeof(questions)/sizeof(questions[0]);
+    char guess;
+    int score;
+    
+    for(int i = 0; i < size; i++){
+        cout << "******************************************************** << \n";
+        cout << questions[i] << '\n';
+        cout << "******************************************************** << \n";
+        
+        for(int j = 0; j < sizeof(choices[i])/sizeof(choices[i][0]); j++){
+            cout << choices[i][j] << '\n';
+        }
+        
+        cin >> guess;
+        guess = toupper(guess);
+        
+        if(guess == answersKey[i]){
+            cout << "CORRECT HOMIE\n";
+            score++;
+        }
+        else{
+            cout << "WRONG SUCKA!!!\n";
+            cout << "Answer: " << answerskey[i] << '\n';
+        }
+        
+        
+    }
+        cout << "********************************************************\n";
+        cout << "                      RESULTS                          * \n";
+        cout << "******************************************************** \n";
+        cout << " CORRECT GUESSES: " << score << '\n';
+        cout << "# of QUESTIONS: " << size << '\n';
+        cout << "SCORE: " << (score/(double)size)*100 << "%" ;
+    
+    return 0;
+}
+
